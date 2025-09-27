2f9cd08641d92cc22eb248e9f84d02827a0c0d0e
LJWsao <112007778+LJWsao@users.noreply.github.com>
2025-09-26 20:44:39 -0400

    Add files via upload

--- hw0/src/fibonacci.c
+++ hw0/src/fibonacci.c
@@ -2,12 +2,21 @@
 #include <stdlib.h>
 
 void usage(char *arg_0) {
-  printf("usage: ./%s <n>\nn -- Compute the nth Fibonacci number, n>=0");
+  printf("usage: ./%s <n>\nn -- Compute the nth Fibonacci number, n>=0", arg_0);
   return;
 }
 
 int Fibonacci(int n) {
   // //your solution here!
+ if (n == 0) {
+    return 0;
+ } 
+ return Fibonacci(n-2) + Fibonacci(n-1);
+/*
+ * Note that the 47th fibonacci number outputs a negative value because
+ * it is larger than the maximum value for a 32 bit (4 byte) signed integer.
+ * As a result there is a integer overflow that causes the value to wrap and become negative.
+ */
 }
 
 int main(int argc, char *argv[]) {
@@ -17,7 +26,7 @@ int main(int argc, char *argv[]) {
     return 0;
   }
   // Parse the value of n here using atoi!
-  // //your solution here!
+  n = atoi(argv[0]);
 
   if (n < 0) {
     usage(argv[0]);
--- hw0/src/myls.c
+++ hw0/src/myls.c
@@ -4,8 +4,12 @@
 void usage(char *argv_0) { printf("Usage: ./%s <optional path>\n", argv_0); }
 
 int main(int argc, char *argv[]) {
+  DIR *dir;
+  struct dirent *direntry; //Will contain directory entry values
+  
   char *default_dir = ".";
   char *target_dir = NULL;
+
   switch (argc) {
   case 1:
     target_dir = default_dir;
@@ -18,5 +22,16 @@ int main(int argc, char *argv[]) {
     return 0;
   };
   // //your solution here!
+  dir = opendir(target_dir);
+  if (dir == NULL) {
+     printf("Directory failed to open");
+     return (0);
+  }
+  dirent = readdir(dir);
+  while (dirent != NULL) { 
+    printf("%s\n", dirent->d_name);
+    dirent = readdir(dir);
+  }
+  closedir(dir);
   return (0);
 }
